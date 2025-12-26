d5714aac364d0d383c3a6cec3d8a8a4900043edc
Carlos Avila <avila966@gmail.com>
2025-12-26 13:20:46 -0600

    final commit

--- src/Main.java
+++ src/Main.java
@@ -1,39 +1,76 @@
 import java.util.Scanner;
 
 public class Main {
+    static Scanner scanner = new Scanner(System.in);
 
-    public static void main(String[] args){
+    public static void main(String[] args) {
 
 
-        Scanner scanner = new Scanner(System.in);
-        double balance;
+        double balance = 0;
         boolean isRunning = true;
         int choice;
 
-        System.out.println("***************");
-        System.out.println("BANKING PROGRAM");
-        System.out.println("***************");
-        System.out.println("1. Show Balance");
-        System.out.println("2. Deposit");
-        System.out.println("3. Withdraw");
-        System.out.println("4. Exit");
-        System.out.println("***************");
+        while (isRunning) {
+            System.out.println("***************");
+            System.out.println("BANKING PROGRAM");
+            System.out.println("***************");
+            System.out.println("1. Show Balance");
+            System.out.println("2. Deposit");
+            System.out.println("3. Withdraw");
+            System.out.println("4. Exit");
+            System.out.println("***************");
 
 
-        System.out.print("Enter your choice (1-4): ");
-        choice = scanner.nextInt();
+            System.out.print("Enter your choice (1-4): ");
+            choice = scanner.nextInt();
 
-        switch(choice){
-            case 1 -> System.out.println("SHOW BALANCE");
-            case 2 -> System.out.println("DEPOSIT");
-            case 3 -> System.out.println("WITHDRAW");
-            case 4 -> isRunning = false;
-            default -> System.out.println("INVALID CHOICE");
+            switch (choice) {
+                case 1 -> showBalance(balance);
+                case 2 -> balance += deposit();
+                case 3 -> balance -= withdraw(balance);
+                case 4 -> isRunning = false;
+                default -> System.out.println("INVALID CHOICE");
+            }
+
         }
-
-
-
+        System.out.println("***************************");
+        System.out.println("Thank you! Have a nice day!");
+        System.out.println("***************************");
 
         scanner.close();
+    }
+
+    static void showBalance(double balance) {
+        System.out.println("***************");
+        System.out.printf("$%.2f\n", balance);
+    }
+
+    static double deposit() {
+        double amount;
+        System.out.print("Enter an amount to be deposited: ");
+        amount = scanner.nextDouble();
+
+        if (amount < 0) {
+            System.out.println("Amount can't be negative");
+            return 0;
+        } else {
+            return amount;
+        }
+    }
+
+    static double withdraw(double balance) {
+        System.out.print("Enter amount to be withdrawn: ");
+        double amount;
+        amount = scanner.nextDouble();
+
+        if (amount > balance) {
+            System.out.println("INSUFFICIENT FUNDS");
+            return 0;
+        } else if (amount < 0) {
+            System.out.println("Amount can't be negative");
+            return 0;
+        } else {
+            return amount;
+        }
     }
 }
